f437dc5d2d51940bc29a856bd26d1794458f6f43
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-06-22 23:32:15 +0530

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA09jQgHCXfg0hNH085BSqZ3jNXEIItRys",
-  authDomain: "cold-chain-project-767f2.firebaseapp.com",
-  projectId: "cold-chain-project-767f2",
-  storageBucket: "cold-chain-project-767f2.firebasestorage.app",
-  messagingSenderId: "555465093103",
-  appId: "1:555465093103:web:d2928f3247c8fcf562d01a",
-  measurementId: "G-NQD9NRF2H1"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
